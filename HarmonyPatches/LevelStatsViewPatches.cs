ebaad576155db250cb3d31de7985b22bf03387d2
PulseLane <291593+PulseLane@users.noreply.github.com>
2022-02-19 10:42:43 -0500

    Update the score/percentage in ShowStats instead of RefreshContent

--- HarmonyPatches/LevelStatsViewPatches.cs
+++ HarmonyPatches/LevelStatsViewPatches.cs
@@ -6,15 +6,38 @@ namespace ScorePercentage.HarmonyPatches
     [HarmonyPatch("ShowStats", MethodType.Normal)]
     class LevelStatsViewPatches : LevelStatsView
     {
-        static void Postfix(ref LevelStatsViewPatches __instance)
+        static void Postfix(ref LevelStatsViewPatches __instance, IDifficultyBeatmap difficultyBeatmap, PlayerData playerData)
         {
             //Update highScoreText, if enabled in Plugin Config
             if (PluginConfig.Instance.EnableMenuHighscore)
             {
-                Plugin.log.Debug("Adding Percentage to HighscoreText");
-                __instance._highScoreText.text = Plugin.scorePercentageCommon.currentScore.ToString() + " " + "(" + Plugin.scorePercentageCommon.currentPercentage.ToString() + "%)";
-            }
+                if (playerData != null)
+                {
+                    PlayerLevelStatsData playerLevelStatsData = playerData.GetPlayerLevelStatsData(difficultyBeatmap.level.levelID, difficultyBeatmap.difficulty, difficultyBeatmap.parentDifficultyBeatmapSet.beatmapCharacteristic);
+                    Plugin.scorePercentageCommon.currentScore = playerLevelStatsData.highScore;
 
+                    //Prepare Data for LevelStatsView
+                    if (playerLevelStatsData.validScore)
+                    {
+                        Plugin.log.Debug("Condition 2 is true");
+                        //calculate maximum possilble score
+                        int currentDifficultyMaxScore = ScorePercentageCommon.calculateMaxScore(difficultyBeatmap.beatmapData.cuttableNotesCount);
+                        //calculate actual score percentage
+                        Plugin.scorePercentageCommon.currentPercentage = ScorePercentageCommon.calculatePercentage(currentDifficultyMaxScore, playerLevelStatsData.highScore);
+                    }
+                    else
+                    {
+                        Plugin.scorePercentageCommon.currentPercentage = 0;
+                    }
+
+                    Plugin.log.Debug("Adding Percentage to HighscoreText");
+                    __instance._highScoreText.text = Plugin.scorePercentageCommon.currentScore.ToString() + " " + "(" + Plugin.scorePercentageCommon.currentPercentage.ToString() + "%)";
+                }
+                else
+                {
+                    Plugin.log.Debug("Player data was null");
+                }
+            }
         }
     }
 }
